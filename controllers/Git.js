724d2fcd7948f4f5ef148f19b7e9ca9e60c9255c
salman <syedsalmanhassan37@gmail.com>
2023-05-02 10:30:12 +0500

    Latest Commit

--- controllers/Git.js
+++ controllers/Git.js
@@ -3,44 +3,39 @@ const dotenv                    = require("dotenv");
 dotenv.config();
 const token                     = process.env.AUTHORIZATION;
 const User                      = require("../models/User");
-let Users                       = new Array();
-Users.fill(0);
+
+
 module.exports.gitdata = async (req, res, next) => {
     try {
-        const resp = await axios.get("https://api.github.com/users", {
-            headers: {
-                Authorization   : `token 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`,
-            },
-        });
-        Users                   = resp.data;
-        next();  
-  }
-  catch (error) {
-    console.log(error.message);
-  }
+            const resp = await axios.get("https://api.github.com/users", {
+                headers: {
+                    Authorization   : `token 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`,
+                },
+            });
+        if (resp)
+        {   
+             res.status(200).send({
+                Message: "Fetch Successful",
+                Data:resp.data    
+            })
+        }
+        else {
+             res.status(500).send({
+                Message: "Fetch Failed",
+                Data:resp.data    
+            })
+        }
+        return res;
+        } catch (error) {
+            return res.status(501).send({ Message: error.Message });
+        }
 }
 
-module.exports.UserCreation = async(req, res, next) =>{
+module.exports.UserCreation = async(data) =>{
     try {
-        for (let i = 0; i < Users.length; i++)
-        {
-            const user = new User({
-                login: Users[i].login,
-                node_id: Users[i].node_id,
-                avatar_url: Users[i].avatar_url,
-            })
-            const resp = await user.save();
-            if (resp)
-            {
-                return res.status(200).send({Message:"Users Created"})
-            }
-            else
-            {
-                return res.status(403).send({Message:"Users not Created"})
-            }
-            }
+           
     } catch (error) {
-        return res.status(500).send({ Message: "Internal Error" });
+        return res.status(500).send({ Message: error.Message });
     }
 }
 
